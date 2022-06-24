5a6db57cf21181de335ed8a2c4b126356d7784da
Keith Radford <53020925+keithradford@users.noreply.github.com>
2022-06-24 15:36:46 -0700

    Connect to Algorithm 1 and 2 APIs (#41)
    Connect to algorithm 1 and 2 apis

--- src/types/Schedule.ts
+++ src/types/Schedule.ts
@@ -1,4 +1,5 @@
 import { arg, extendType, inputObjectType, intArg, nonNull, objectType } from 'nexus';
+import fetch from 'node-fetch';
 import { CourseSection } from './Course/Section';
 import { Date } from './Date';
 import { Response } from './Response';
@@ -37,7 +38,133 @@ export const ScheduleMutation = extendType({
       args: {
         input: arg({ type: nonNull(GenerateScheduleInput) }),
       },
-      resolve: () => ({ success: false, message: 'Not implemented' }),
+      resolve: async () => {
+        const response = await fetch('https://seng499company4algorithm1.herokuapp.com/generate_schedule', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+          },
+          body: JSON.stringify({
+            fallTermCourses: [
+              {
+                courseNumber: 'string',
+                subject: 'string',
+                sequenceNumber: 'string',
+                courseTitle: 'string',
+                requiredEquipment: ['string'],
+                streamSequence: 'string',
+                meetingTime: {
+                  startDate: 'string',
+                  endDate: 'string',
+                  beginTime: 'string',
+                  endtime: 'string',
+                  hoursWeek: 0,
+                  sunday: true,
+                  monday: true,
+                  tuesday: true,
+                  wednesday: true,
+                  thursday: true,
+                  friday: true,
+                  saturday: true,
+                },
+                prof: {
+                  prefs: [
+                    {
+                      courseNum: 'string',
+                      preferenceNum: 0,
+                      term: 'string',
+                    },
+                  ],
+                  displayName: 'string',
+                  requiredEquipment: ['string'],
+                  fallTermCourses: 0,
+                  springTermCourses: 0,
+                  summerTermCourses: 0,
+                },
+              },
+            ],
+            springTermCourses: [
+              {
+                courseNumber: 'string',
+                subject: 'string',
+                sequenceNumber: 'string',
+                courseTitle: 'string',
+                requiredEquipment: ['string'],
+                streamSequence: 'string',
+                meetingTime: {
+                  startDate: 'string',
+                  endDate: 'string',
+                  beginTime: 'string',
+                  endtime: 'string',
+                  hoursWeek: 0,
+                  sunday: true,
+                  monday: true,
+                  tuesday: true,
+                  wednesday: true,
+                  thursday: true,
+                  friday: true,
+                  saturday: true,
+                },
+                prof: {
+                  prefs: [
+                    {
+                      courseNum: 'string',
+                      preferenceNum: 0,
+                      term: 'string',
+                    },
+                  ],
+                  displayName: 'string',
+                  requiredEquipment: ['string'],
+                  fallTermCourses: 0,
+                  springTermCourses: 0,
+                  summerTermCourses: 0,
+                },
+              },
+            ],
+            summerTermCourses: [
+              {
+                courseNumber: 'string',
+                subject: 'string',
+                sequenceNumber: 'string',
+                courseTitle: 'string',
+                requiredEquipment: ['string'],
+                streamSequence: 'string',
+                meetingTime: {
+                  startDate: 'string',
+                  endDate: 'string',
+                  beginTime: 'string',
+                  endtime: 'string',
+                  hoursWeek: 0,
+                  sunday: true,
+                  monday: true,
+                  tuesday: true,
+                  wednesday: true,
+                  thursday: true,
+                  friday: true,
+                  saturday: true,
+                },
+                prof: {
+                  prefs: [
+                    {
+                      courseNum: 'string',
+                      preferenceNum: 0,
+                      term: 'string',
+                    },
+                  ],
+                  displayName: 'string',
+                  requiredEquipment: ['string'],
+                  fallTermCourses: 0,
+                  springTermCourses: 0,
+                  summerTermCourses: 0,
+                },
+              },
+            ],
+          }),
+        });
+
+        return { success: true, message: JSON.stringify(await response.json()) };
+      },
     });
   },
 });
@@ -45,12 +172,34 @@ export const ScheduleMutation = extendType({
 export const ScheduleQuery = extendType({
   type: 'Query',
   definition(t) {
-    t.field('schedule', {
-      type: Schedule,
+    t.string('schedule', {
+      // TODO: Revert this, just wanna return proof we connected to algo 2
+      // type: Schedule,
       description:
         'Schedule for a given term. If year is given, returns the most recent schedule generated for that year.',
       args: {
         year: intArg(),
+      },
+      resolve: async () => {
+        const response = await fetch('https://seng499company4algorithm2.herokuapp.com/predict_class_size', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+          },
+          body: JSON.stringify([
+            {
+              subject: 'SENG',
+              code: '499',
+              // eslint-disable-next-line camelcase
+              seng_ratio: 0.75,
+              semester: 'FALL',
+              capacity: 45,
+            },
+          ]),
+        });
+
+        return JSON.stringify(await response.json());
       },
     });
   },
