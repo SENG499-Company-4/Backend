3b681a8f694f0bd0be604da492ff9149b727ce20
Keith Radford <khr8771@gmail.com>
2022-07-07 10:48:05 -0700

    DB migrations and edit seeding script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, Term, Peng, Role } from '@prisma/client';
+import { partition, randomNumber } from '../src/utils/helpers';
 import courseData from './static/courses.json';
 import userData from './static/users.json';
 const prisma = new PrismaClient();
@@ -6,12 +7,38 @@ const prisma = new PrismaClient();
 async function main() {
   if ((await prisma.user.count()) === 0) {
     for (const userObj of userData) {
+      const preferences = courseData.map((__, i) => ({
+        courseID: i + 1,
+        rank: randomNumber(6),
+      }));
+
+      // Randomly distribute either 5 or 3 courses across the 3 terms
+      const { maxCoursesFall, maxCoursesSpring, maxCoursesSummer } = partition(randomNumber(8) > 4 ? 3 : 5);
+
       await (prisma as PrismaClient).user.create({
         data: {
           name: userObj.name,
           username: userObj.username,
           password: userObj.password,
           role: userObj.role as Role,
+          peng: randomNumber(8) > 4,
+          preferences: {
+            create: preferences,
+          },
+          professorSettings: {
+            create: [
+              {
+                year: 2021,
+                maxCoursesFall,
+                maxCoursesSpring,
+                maxCoursesSummer,
+                hasRelief: randomNumber(8) > 4,
+                reliefReason: 'Release reason',
+                hasTopic: randomNumber(8) > 4,
+                topicDescription: 'TOPIC',
+              },
+            ],
+          },
         },
       });
     }
