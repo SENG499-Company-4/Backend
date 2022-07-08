454590e608be52a83798e8ff02084f61cd34928c
Eduardo Szeckir <50898635+szeckirjr@users.noreply.github.com>
2022-07-08 14:04:18 -0700

    Fix/gen schedule mutation (#56)
    * Adding term and courses list to generate schedule
    
    * New graphql schema
    
    * Fix return type of preference query
    
    * Change course id input
    
    * Add company and algorithms to gen schedule
    
    * some fixes
    
    * revert preference changes
    
    * Rename
    
    * Fix lint
    
    * Fix lint
    
    * courses type fix

--- src/types/Course/ID.ts
+++ src/types/Course/ID.ts
@@ -1,4 +1,4 @@
-import { objectType } from 'nexus';
+import { inputObjectType, objectType } from 'nexus';
 import { Term } from '../Term';
 
 export const CourseID = objectType({
@@ -11,5 +11,14 @@ export const CourseID = objectType({
       description: 'Term course is offered in',
     });
     t.nonNull.int('year', { description: 'Year course is offered in' });
+  },
+});
+
+export const CourseInput = inputObjectType({
+  name: 'CourseInput',
+  definition(t) {
+    t.nonNull.string('subject', { description: 'Course subject, e.g. SENG, CSC' });
+    t.nonNull.string('code', { description: 'Course code, e.g. 499, 310' });
+    t.nonNull.int('section', { description: 'Number of sections in the course' });
   },
 });
