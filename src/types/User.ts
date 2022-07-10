9c8bf73d22b656829ded051189ad46b1d167e93b
Eduardo Szeckir <50898635+szeckirjr@users.noreply.github.com>
2022-07-09 17:10:19 -0700

    Adding allUser query (#59)

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { compare } from 'bcrypt';
-import { arg, enumType, extendType, idArg, inputObjectType, intArg, nonNull, objectType, stringArg } from 'nexus';
+import { arg, enumType, extendType, idArg, inputObjectType, intArg, list, nonNull, objectType, stringArg } from 'nexus';
 import { generateToken, getUserId } from '../utils/auth';
 import generateHashPassword from '../utils/hash';
 import { CoursePreference } from './Course/Preference';
@@ -257,6 +257,13 @@ export const UserQuery = extendType({
             id,
           },
         });
+      },
+    });
+    t.field('allUsers', {
+      type: list(nonNull(User)),
+      description: 'Get all users',
+      resolve: async (_, __, { prisma }) => {
+        return await (prisma as PrismaClient).user.findMany();
       },
     });
   },
