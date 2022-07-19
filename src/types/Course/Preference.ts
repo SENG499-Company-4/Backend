5f30c0d6ea7637d162849f651d024e21d2cd1701
Peter Wilson <57307032+peterrwilson99@users.noreply.github.com>
2022-07-19 11:09:16 -0600

    Preference Queries and Mutation (#71)
    * Adding changes from preference-mutation branch
    
    * Removing subject FAKE from !courseData in query
    
    * Fix build problem
    
    * ProfessorSettings id change, and move to upsert
    
    * Adding preferences to user model, contains error on 251
    
    * Merge changes and apply fixes
    
    * maxCapacity nullable changes
    
    * Fix some changes
    
    * Remove console logs
    
    * Removal of year from CoursePreferenceInput
    
    Co-authored-by: Peter Wilson <peterrwilson99.com>
    Co-authored-by: Eduardo Szeckir <eszeckirjr@gmail.com>
    Co-authored-by: Keith Radford <khr8771@gmail.com>

--- src/types/Course/Preference.ts
+++ src/types/Course/Preference.ts
@@ -1,6 +1,9 @@
+/* eslint-disable camelcase */
+import { arg, extendType, inputObjectType, nonNull, objectType } from 'nexus';
 import { PrismaClient, Term } from '@prisma/client';
-import { extendType, objectType } from 'nexus';
 import { getUserId } from '../../utils/auth';
+import { Response } from '../Response';
+import { Term as TermType } from '../Term';
 import { CourseID } from './ID';
 
 export const CoursePreference = objectType({
@@ -20,6 +23,40 @@ export const TeachingPreferenceSurvey = objectType({
   },
 });
 
+export const CoursePreferenceInput = inputObjectType({
+  name: 'CoursePreferenceInput',
+  definition(t) {
+    t.nonNull.string('subject', { description: 'Course subject, e.g. SENG, CSC' });
+    t.nonNull.string('code', { description: 'Course code, e.g. 499, 310' });
+    t.nonNull.field('term', {
+      type: TermType,
+      description: 'Term course is offered in',
+    });
+    t.nonNull.int('preference');
+  },
+});
+
+export const CreateTeachingPreferenceInput = inputObjectType({
+  name: 'CreateTeachingPreferenceInput',
+  definition(t) {
+    t.nonNull.boolean('peng');
+    t.nonNull.id('userId');
+    t.nonNull.list.nonNull.field('courses', {
+      type: CoursePreferenceInput,
+    });
+    t.field('nonTeachingTerm', {
+      type: TermType,
+    });
+    t.nonNull.boolean('hasRelief');
+    t.string('reliefReason');
+    t.nonNull.boolean('hasTopic');
+    t.string('topicDescription');
+    t.int('fallTermCourses');
+    t.int('springTermCourses');
+    t.int('summerTermCourses');
+  },
+});
+
 export const PreferenceQuery = extendType({
   type: 'Query',
   definition(t) {
@@ -27,45 +64,233 @@ export const PreferenceQuery = extendType({
       type: TeachingPreferenceSurvey,
       description: 'Get Teaching Preference Survey for the current user',
       resolve: async (_, __, { prisma, token }) => {
+        const prefs = await Promise.all(
+          await (prisma as PrismaClient).preference.findMany({
+            where: {
+              user: {
+                id: getUserId(token),
+              },
+            },
+          })
+        );
+
+        const courses = await Promise.all(
+          prefs.map(async ({ courseID, rank }) => {
+            const courseData = await (prisma as PrismaClient).course.findUnique({
+              where: {
+                id: courseID,
+              },
+            });
+
+            if (!courseData) {
+              return {
+                id: {
+                  subject: '',
+                  code: '',
+                  term: '' as Term,
+                  year: 0,
+                },
+                preference: 0,
+              };
+            }
+            return {
+              id: {
+                subject: courseData.subject,
+                code: courseData.code,
+                term: courseData.term,
+                year: courseData.year,
+              },
+              preference: rank ?? 0,
+            };
+          })
+        );
+        return {
+          courses,
+        };
+      },
+    });
+  },
+});
+
+export const AllPreferencesQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.list.nonNull.field('coursePreferences', {
+      type: CoursePreference,
+      description: 'Get all courses preferences',
+      resolve: async (_, __, { prisma }) => {
+        const prefs = await (prisma as PrismaClient).preference.findMany();
+        const courses = await Promise.all(
+          prefs.map(async ({ courseID, rank }) => {
+            const courseData = await (prisma as PrismaClient).course.findUnique({
+              where: {
+                id: courseID,
+              },
+            });
+            if (!courseData) {
+              return {
+                id: {
+                  subject: '',
+                  code: '',
+                  term: 'FALL' as Term,
+                  year: 0,
+                },
+                preference: 0,
+              };
+            }
+            return {
+              id: {
+                subject: courseData.subject,
+                code: courseData.code,
+                term: courseData.term,
+                year: courseData.year,
+              },
+              preference: rank ?? 0,
+            };
+          })
+        );
+        return courses;
+      },
+    });
+  },
+});
+
+export const PreferenceMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('createTeachingPreference', {
+      type: Response,
+      description: 'Teaching preferences',
+      args: {
+        input: arg({ type: nonNull(CreateTeachingPreferenceInput) }),
+      },
+      resolve: async (
+        _,
+        {
+          input: {
+            peng,
+            userId,
+            courses,
+            hasRelief,
+            reliefReason,
+            hasTopic,
+            topicDescription,
+            fallTermCourses,
+            springTermCourses,
+            summerTermCourses,
+          },
+        },
+        { prisma }
+      ) => {
+        const preferenceObjs = await Promise.all(
+          courses.map(
+            async ({
+              subject,
+              code,
+              term,
+              preference,
+            }: {
+              subject: string;
+              code: string;
+              term: Term;
+              preference: number;
+            }) => {
+              const courseData = await (prisma as PrismaClient).course.upsert({
+                where: {
+                  subject_code_year_term: {
+                    subject: subject,
+                    code: code,
+                    term: term,
+                    year: new Date().getFullYear(),
+                  },
+                },
+                update: {},
+                create: {
+                  subject: subject,
+                  code: code,
+                  term: term,
+                  year: new Date().getFullYear(),
+                },
+              });
+
+              const courseID = courseData ? courseData.id : -1; //if courseData is null set id to -1
+
+              return {
+                courseID: courseID,
+                rank: preference ?? 0,
+              };
+            }
+          )
+        );
+
+        for (const preferenceObj of preferenceObjs) {
+          await (prisma as PrismaClient).preference.upsert({
+            where: {
+              userID_courseID: {
+                userID: Number(userId),
+                courseID: preferenceObj.courseID,
+              },
+            },
+            update: {
+              rank: preferenceObj.rank,
+            },
+            create: {
+              user: { connect: { id: Number(userId) } },
+              course: { connect: { id: preferenceObj.courseID } },
+              rank: preferenceObj.rank,
+            },
+          });
+        }
+
         const prefs = await (prisma as PrismaClient).preference.findMany({
           where: {
             user: {
-              id: getUserId(token),
+              id: Number(userId),
             },
           },
         });
 
-        const courses = prefs.map(async ({ courseID, rank }) => {
-          const courseData = await (prisma as PrismaClient).course.findUnique({
-            where: {
-              id: courseID,
+        await (prisma as PrismaClient).user.update({
+          where: {
+            id: Number(userId),
+          },
+          data: {
+            peng: peng,
+            preferences: {
+              set: prefs.map(({ userID, courseID }) => ({ userID_courseID: { userID, courseID } })),
             },
-          });
-
-          if (!courseData) {
-            return {
-              id: {
-                subject: '',
-                code: '',
-                term: '' as Term,
-                year: 0,
-              },
-              preference: 0,
-            };
-          }
-          return {
-            id: {
-              subject: courseData.subject,
-              code: courseData.code,
-              term: courseData.term,
-              year: courseData.year,
-            },
-            preference: rank ?? 0,
-          };
+          },
         });
-        return {
-          courses,
-        };
+
+        await (prisma as PrismaClient).professorSettings.upsert({
+          where: {
+            year_userID: {
+              year: new Date().getFullYear(),
+              userID: Number(userId),
+            },
+          },
+          update: {
+            maxCoursesFall: fallTermCourses ?? 0,
+            maxCoursesSpring: springTermCourses ?? 0,
+            maxCoursesSummer: summerTermCourses ?? 0,
+            hasRelief: hasRelief,
+            reliefReason: reliefReason ?? '',
+            hasTopic: hasTopic,
+            topicDescription: topicDescription ?? '',
+          },
+          create: {
+            userID: Number(userId),
+            year: new Date().getFullYear(),
+            maxCoursesFall: fallTermCourses ?? 0,
+            maxCoursesSpring: springTermCourses ?? 0,
+            maxCoursesSummer: summerTermCourses ?? 0,
+            hasRelief: hasRelief,
+            reliefReason: reliefReason ?? '',
+            hasTopic: hasTopic,
+            topicDescription: topicDescription ?? '',
+          },
+        });
+        return { success: true, message: 'Preferences Added' };
       },
     });
   },
