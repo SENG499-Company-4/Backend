c1b4805dd9de22cbfaea30395ae375edfdb2b9ff
Keith Radford <khr8771@gmail.com>
2022-07-21 11:29:37 -0700

    Lil fixes

--- src/schema.ts
+++ src/schema.ts
@@ -5,7 +5,7 @@ import * as types from './types';
 export const schema = makeSchema({
   types,
   outputs: {
-    typegen: join(__dirname, '.', 'types.ts'),
+    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
     schema: join(__dirname, '..', 'schema.graphql'),
   },
   contextType: {
